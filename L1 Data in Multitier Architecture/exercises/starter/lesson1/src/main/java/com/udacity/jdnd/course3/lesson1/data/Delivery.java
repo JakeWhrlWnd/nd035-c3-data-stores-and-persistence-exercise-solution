88b0250b12f004ce3ef83b1c1af5c18f5860be74
JakeWhrlWnd <justjamescreative@outlook.com>
2022-11-17 00:47:14 -0500

    Created the Entities for Flower and Delivery
    Added necessary annotations

---
+++ L1 Data in Multitier Architecture/exercises/starter/lesson1/src/main/java/com/udacity/jdnd/course3/lesson1/data/Delivery.java
@@ -0,0 +1,66 @@
+package com.udacity.jdnd.course3.lesson1.data;
+
+import org.hibernate.annotations.Nationalized;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Delivery {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized
+    private String name;
+    @Column(name = "address_full", length = 500)
+    private String address;
+    private LocalDateTime deliveryTime;
+    @Type(type = "yes_no")
+    private boolean completed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(LocalDateTime deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
