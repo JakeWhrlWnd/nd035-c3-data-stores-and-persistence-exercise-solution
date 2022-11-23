e071081253bfb54206361003b1b99d7210a24010
JakeWhrlWnd <justjamescreative@outlook.com>
2022-11-22 22:43:52 -0500

    Updated the PlantRepository, PlantService, and PlantController

--- L1 Data in Multitier Architecture/exercises/starter/lesson1/src/main/java/com/udacity/jdnd/course3/lesson1/controller/PlantController.java
+++ L1 Data in Multitier Architecture/exercises/starter/lesson1/src/main/java/com/udacity/jdnd/course3/lesson1/controller/PlantController.java
@@ -6,8 +6,13 @@ import com.udacity.jdnd.course3.lesson1.data.PlantDTO;
 import com.udacity.jdnd.course3.lesson1.service.PlantService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/plant")
@@ -15,13 +20,14 @@ public class PlantController {
     @Autowired
     private PlantService plantService;
 
-    public PlantDTO getPlantDTO(String name) {
-        return convertPlantToPlantDTO(plantService.getPlantByName(name));
+    @GetMapping("/delivered/{id}")
+    public Boolean delivered(@PathVariable Long id) {
+        return plantService.delivered(id);
     }
-
+    @GetMapping("/under-price/{price}")
     @JsonView(Views.Public.class)
-    public Plant getFilteredPlant(String name) {
-        return plantService.getPlantByName(name);
+    public List<Plant> plantCheaperThan(@PathVariable BigDecimal price) {
+        return plantService.findPlantsBelowPrice(price);
     }
 
     private PlantDTO convertPlantToPlantDTO(Plant plant) {
